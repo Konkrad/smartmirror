d26b3d9463db781fdcaafaff19080ada219703f8
Paul Sonnentag <Paul.Sonnentag@googlemail.com>
2015-09-26 14:20:06 +0200

    Add widget

---
+++ www/js/widget.js
@@ -0,0 +1,25 @@
+(function (exports) {
+  'use strict';
+
+  var app = document.getElementById("app");
+  var target = document.getElementById("widget");
+  var destructor = null;
+
+  exports.widget = {
+    show: function (widget) {
+      var args = Array.prototype.slice.call(arguments, 1);
+
+      if (_.isFunction(destructor)) {
+	destructor;
+      }
+
+      destructor  = widget.apply(target, args);
+      app.classList.add("show-widget");
+    },
+
+    hide: function () {
+      app.classList.remove("show-widget");
+    }
+  };
+
+}(window.smartMirror || (window.smartMirror = {})));
