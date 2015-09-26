949ac6f1d5db872b92ad5356ac14095b5c8232d0
Konrad Kleeberger <kkleeberger.mmt-b2014@fh-salzburg.ac.at>
2015-09-26 20:18:12 +0200

    api giphy added

---
+++ www/js/giphy.js
@@ -0,0 +1,34 @@
+(function (exports) {
+  'use strict';
+
+  exports.giphyWidget = function (el) {
+    var template = _.template($("#giphy-template").html());
+
+    var endpoint = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=funny";
+
+    var giphy = {
+      url: "img/giphy.gif"
+    }
+
+    var render = function() {
+      $.getJSON( endpoint, function() {
+        })
+        .done(function(data) {
+          if(data.meta.status === 200)
+          {
+            giphy.url = data.data.image_original_url;
+          }
+        })
+        .always(function() {
+          el.html(template({
+            url: giphy.url
+          }));
+        });
+    };
+
+    render();
+
+    var giphyId = window.setInterval(render, 1000*60*60);
+
+  }
+}(window.smartMirror || (window.smartMirror = {})));
